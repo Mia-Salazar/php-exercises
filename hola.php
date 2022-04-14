60c5fa96ccae07da33f802bc4c2f4655ff97e923
Mia-Salazar <maria.sgr@gmail.com>
2022-04-14 16:22:34 +0200

    Practice with PHP and HTML

--- hola.php
+++ hola.php
@@ -1,3 +1,29 @@
-<?php
-echo "Hola mundito";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Random title</title>
+</head>
+<body>
+	<?php 
+		$title = "This is a title";
+		$image = "https://xcjgames.com/assets/logo.png";
+		$alt = "Alt text";
+		$webXCJ = "xcjgames.com";
+		$web = "miasalazar";
+		$netflify = ".netlify.app";
+	?>
+	<h1><?php echo $title ?></h1>
+	<?php 
+		$subtitle = "This is a subtitle";
+		echo "<h2>" . $subtitle . "</h2>";
+	?>
+	<figure style="max-width: 50%;">
+		<img style="width: 100%;" src="<?php echo $image ?>" alt="<?php echo $alt ?>"/>
+	</figure>
+	<a target="_blank" href="<?php echo 'http://' . $web . $netflify ?>">Link to miasalazar.com</a>
+	<a target="_blank" href="<?php echo 'https://' . $webXCJ ?>">Link to XCJ Games</a>
+
+</body>
+</html>
